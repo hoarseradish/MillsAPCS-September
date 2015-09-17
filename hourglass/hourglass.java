ad1a7dfbd4e9ef7335c744c765fcbffd9db204bb
hoarseradish <trex_gavin@yahoo.com>
2015-09-17 08:53:26 -0700

    9/17

---
+++ hourglass/hourglass.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class hourglass here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class hourglass
+{
+    public static void main(String[] args) {
+        for (int i = 1; i<=4; i++){
+            
+        
+        
+        
+        
+        
+    }
+}
+    public static void doubleColon() {
+        System.out.println("::");
+}
+public static void top()  {
+    System.outlprintln("|\"\"\"\"\"\"\"\"\"\"|");
+}
+ 
