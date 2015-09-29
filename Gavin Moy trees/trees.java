7063e922b1e5865df4d18ba56c7cabcbcf4908b9
hoarseradish <trex_gavin@yahoo.com>
2015-09-29 09:36:08 -0700

    9/29

--- Gavin Moy trees/trees.java
+++ Gavin Moy trees/trees.java
@@ -8,16 +8,20 @@
 public class trees
 {
     public static void main(String[] args) {
+        writeTree(1,7);
+        writeTree(5,6);
+        writeTree(4,3);
         writeTree(3,4);
-        writeTree(2,5);
+        writeTree(6,8);
+        writeTree(4,9);
+        writeTree(4,4);
 
-       
     }
-
     public static void writeTree( int x, int y) { //x = segments, y = height
-        for (int i= 1; i<=x ; i++ ) {
+        int z = y;
+        for (int i= 1; i<=x ; i++, y++) {
             for (int j=i; j<=y; j++) {
-                for (int k=j; k<=5; k++) {
+                for (int k=j; k<=(x+z); k++) {
                     System.out.print(" ");
                 }
                 System.out.print("*");
@@ -28,18 +32,20 @@ public class trees
             }
 
         }
-        bottom();
+        for (int n=1 ; n<=2; n++) {
+            for (int m= 1; m<= x+z; m++) {
+                System.out.print(" ");
+            }
+            System.out.println("*");
+        }
+        for (int o=1; o<=((x+z)-3); o++){
+            System.out.print(" ");
+        }
+        System.out.println("*******");
     }
 
     public static void doubleStar() {
         System.out.print("**");
     }
 
-    public static void bottom() {
-        System.out.println("     *");
-        System.out.println("     *");
-        System.out.println("  *******");
-        System.out.println();
-
-    }
 }
